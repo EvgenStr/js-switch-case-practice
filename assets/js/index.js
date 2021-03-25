2ee4b1aa73eecdbb01fcd751ff2ad91c1abf8a99
EvgenStr <juve2910@gmail.com>
2021-03-25 11:23:51 +0200

    add callback functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 let inp = prompt("Enter day (1-31):");
 const num1 = +prompt("Enter number one:");
 const num2 = +prompt("Enter number two:");
-let operation = prompt("Enter operation (+, -, *,/ ):");
+const operation = prompt("Enter operation (+, -, *,/ ):");
 
 switch (true) {
-  case inp >0 && inp <= 10: {
+  case inp > 0 && inp <= 10: {
     console.log(inp, "- first decade");
     break;
   }
@@ -12,7 +12,7 @@ switch (true) {
     console.log(inp, "- second decade");
     break;
   }
-  case inp >20 && inp <= 31: {
+  case inp > 20 && inp <= 31: {
     console.log(inp, "- third decade");
     break;
   }
@@ -21,32 +21,53 @@ switch (true) {
   }
 }
 
-// isNaN(num1 - num2) ? console.log("Invalid number") : calculation();
-console.log(calculation(num1, num1,operation))
-
-/**
- * function calculates values and output value in console
- */
-function calculation(num1, num2, operation) {
-  if (isNaN(num1 - num2)) return false;
-  switch (operation) {
-    case "+": {
-       return `Result: ${num1} + ${num2} = ${num1 + num2}`;
-    }
-    case "-": {
-      return `Result: ${num1} - ${num2} = ${num1 - num2}`;
-    }
-    case "*": {
-      return `Result: ${num1} * ${num2} = ${num1 * num2}`;
-    }
-    case "/": {
-      return `Result: ${num1} / ${num2} = ${num1 / num2}`;
-    }
-    default: {
-      return `Wrong operation:  ${operation}`;
-    }
+switch (true) {
+  case isNaN(num1 - num2): {
+    console.log("error");
+    break;
+  }
+  case operation === "+": {
+    console.log(calc(num1, num2, sum));
+    break;
+  }
+  case operation === "-": {
+    console.log(calc(num1, num2, sub));
+    break;
+  }
+  case operation === "*": {
+    console.log(calc(num1, num2, mul));
+    break;
+  }
+  case operation === "/": {
+    console.log(calc(num1, num2, div));
+    break;
+  }
+  default: {
   }
 }
 
+function calc(a, b, cb) {
+  return cb(a, b);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+
+// isNaN(num1 - num2) ? console.log("Invalid number") : calculation();
+// console.log(calculation(num1, num1,operation))
 // console.log(Boolean(operation));
 // console.log(!isNaN(num1 - num2));
